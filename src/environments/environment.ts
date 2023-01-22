29fdf3aa9d2d95badb987bf2d25503e2f39b8fc7
Alin Florin <alin@qoffice.ro>
2023-01-23 00:37:19 +0200

    -

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyCElEXkyicNJ5cta2GwJK9v4Rwzrrd45ck",
+    authDomain: "avividlogo-dev.firebaseapp.com",
+    projectId: "avividlogo-dev",
+    storageBucket: "avividlogo-dev.appspot.com",
+    messagingSenderId: "466177227653",
+    appId: "1:466177227653:web:0dd955264b6085cc5ea2ec",
+    measurementId: "G-8E3FQBZ149",
+  },
+};
