ad9586f78d6a59c2b42840df211014820cbd529f
Alin Florin <alin@qoffice.ro>
2023-01-25 23:54:13 +0200

    -

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -4,6 +4,7 @@ import { MatDrawer } from '@angular/material/sidenav';
 import { TranslateService } from '@ngx-translate/core';
 import { LocalSettingsService } from 'src/app/services/local-settings.service';
 import { environment } from 'src/environments/environment';
+import { APP_VERSION } from 'src/version';
 
 @Component({
   selector: 'app-header',
@@ -18,6 +19,7 @@ export class HeaderComponent {
   @Output() logoutClicked = new EventEmitter<void>();
 
   allLanguages = environment.ui.language.available;
+  appVersion = APP_VERSION;
 
   constructor(
     public translateService: TranslateService,
